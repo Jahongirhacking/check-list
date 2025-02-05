2e5867630f046a5819e815a69a2e07e9c4d04336
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-02-05 17:59:54 +0500

    feat: add handle send data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,27 @@
 import { HeartFilled, UploadOutlined } from '@ant-design/icons'
 import { Button, Flex, Typography } from 'antd'
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { paths } from '../routes/paths'
+import { RootState } from '../store/store'
 
 const Footer: FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
+    const user = useSelector((store: RootState) => store.user);
+    const navigate = useNavigate();
+
+    const handleSend = async () => {
+        if (user.id) {
+            console.log("sending...");
+        } else {
+            navigate(paths.login);
+        }
+    }
+
     return (
         <footer {...props}>
             <Flex gap={8} vertical align='center'>
-                <Button type='primary' icon={<UploadOutlined />} iconPosition='end'>Telegram orqali yuborish</Button>
+                <Button type='primary' icon={<UploadOutlined />} iconPosition='end' onClick={handleSend}>Telegramga yuborish</Button>
                 <Typography.Text style={{ textAlign: 'center' }}>
                     <a href='https://jahongirhacking.netlify.app/'>Jahongir Hayitov</a>dan sizga taqdim etildi <HeartFilled style={{ color: '#ed0e28' }} />
                 </Typography.Text>
--- src/pages/auth/CallbackHandler.tsx
+++ src/pages/auth/CallbackHandler.tsx
@@ -2,6 +2,7 @@ import { LoadingOutlined } from '@ant-design/icons';
 import React, { useEffect, useState } from 'react';
 import { Navigate, useSearchParams } from 'react-router';
 import { paths } from '../../routes/paths';
+import { telegramBotUsername } from '../../utils/config';
 
 const CallbackHandler = () => {
     const [searchParams] = useSearchParams();
@@ -10,6 +11,7 @@ const CallbackHandler = () => {
     useEffect(() => {
         if (searchParams.has('id')) {
             console.log(searchParams);
+            window.open(`https://t.me/${telegramBotUsername}?start=welcome`, "_blank")
             setSuccess(true);
         }
     }, [searchParams])
