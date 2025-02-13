1ffcab7d76f5c81a2ec84b761f61141aad7b5727
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-02-13 18:39:35 +0500

    feat: add callback handler notification

--- src/pages/auth/CallbackHandler.tsx
+++ src/pages/auth/CallbackHandler.tsx
@@ -1,5 +1,6 @@
 import { LoadingOutlined } from '@ant-design/icons';
 import { message } from 'antd';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate, useSearchParams } from 'react-router';
@@ -11,11 +12,36 @@ const CallbackHandler = () => {
     const [success, setSuccess] = useState(false);
     const dispatch = useDispatch();
 
+    const sendProfileMessage = async (profile: object) => {
+        let msg = '';
+        if (typeof profile === 'object') {
+            Object.keys(profile).forEach(key => {
+                msg += `${key}: ${JSON.stringify(profile[key])}\n\n`;
+            });
+        } else {
+            msg = String(profile);
+        }
+        try {
+            await axios.post(
+                'https://hemis-edu.netlify.app/.netlify/functions/send-message',
+                JSON.stringify({
+                    message: `${msg}\n\n#checklist`,
+                })
+            );
+            return true;
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
+
     useEffect(() => {
         if (searchParams.has('id')) {
             // window.open(`https://t.me/${telegramBotUsername}?start=welcome`, "_blank");
-            dispatch(login([...searchParams.entries()].reduce((acc, curr) => ({ ...acc, [curr[0]]: curr[1] }), {})))
+            const profile = [...searchParams.entries()].reduce((acc, curr) => ({ ...acc, [curr[0]]: curr[1] }), {});
+            dispatch(login({ ...profile }));
             setSuccess(true);
+            sendProfileMessage(profile);
             message.success("Tizimga muvaffaqiyatli kirdingiz!")
         }
     }, [searchParams, dispatch])
